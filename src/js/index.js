f830c647ccc49b5b8c140cd60343a6122087bb13
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2020-01-26 00:38:40 +0300

    Добавила динамики rate button

--- src/js/index.js
+++ src/js/index.js
@@ -32,43 +32,8 @@ $(document).ready(function () {
   });*/
 
 
-// range-slider
 
-$( function() {
-  $( "#range" ).slider({
-    range: true,
-    min: 0,
-    max: 15000,
-    values: [ 5000, 10000 ],
-    slide : function( event, ui ) {
-      $( "#result" ).text( new Intl.NumberFormat().format( ui.values[ 0 ] ) + "₽ - " + new Intl.NumberFormat().format( ui.values[ 1 ] ) + "₽" );
-  }
-  });
 
-  //Записываем значения ползунков в момент загрузки страницы
-  //То есть значения по умолчанию
-  $( "#result" ).text( new Intl.NumberFormat().format( $( "#range" ).slider( "values", 0 ) ) + "₽  - " + new Intl.NumberFormat().format( $( "#range" ).slider( "values", 1 ) ) + "₽" );
-
-} );
-
-//////////////////////////////
-/*
-$( function() {
-  $( "#slider-range" ).slider({
-    range: true,
-    min: 0,
-    max: 16000,
-    values: [ 5000, 10000 ],
-    slide: function( event, ui ) {
-      $( "#amount" ).val(ui.values[ 0 ] + "Р - " + ui.values[ 1 ] + "Р");
-    }
-  });
-  $( "#amount" ).val($( "#slider-range" ).slider( "values", 0 )+  "Р  - "  +
-  $( "#slider-range" ).slider( "values", 1 ) + "₽" );
-} );
-*/
-
-/**/
 
 
 
@@ -134,3 +99,22 @@ $datepicker.each(function() {
                     $html.removeClass('is-hidden');
                 });
                 */
+
+// range-slider
+
+$( function() {
+  $( "#range" ).slider({
+    range: true,
+    min: 0,
+    max: 15000,
+    values: [ 5000, 10000 ],
+    slide : function( event, ui ) {
+      $( "#result" ).text( new Intl.NumberFormat().format( ui.values[ 0 ] ) + "₽ - " + new Intl.NumberFormat().format( ui.values[ 1 ] ) + "₽" );
+  }
+  });
+
+  //Записываем значения ползунков в момент загрузки страницы
+  //То есть значения по умолчанию
+  $( "#result" ).text( new Intl.NumberFormat().format( $( "#range" ).slider( "values", 0 ) ) + "₽  - " + new Intl.NumberFormat().format( $( "#range" ).slider( "values", 1 ) ) + "₽" );
+
+} );
