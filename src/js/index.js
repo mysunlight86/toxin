d73c93abc190e7b4ba6e561c0f6a85079385e890
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2020-01-30 19:51:13 +0300

    Set up recalculation of changes in the drop-down list

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,32 @@
 // Main js file
 
-$(document).ready(function () {
-    $('.iqdropdown').iqDropdown(
-        {
-            // max total items
-            maxItems: Infinity,
-            // min total items
-            minItems: 0,
-            // text to show on the dropdown
-            selectionText: 'гость',
-            // text to show for multiple items
-            textPlural: 'гостя',
-            // text to show default
-            textDefault: 'Cколько гостей',
-            // buttons to increment/decrement
-            controls: {
-              position: 'right',
-              displayCls: 'iqdropdown-item-display',
-              controlsCls: 'iqdropdown-item-controls',
-              counterCls: 'counter'
-            },
-            // fires when an item quantity changes
-            onChange: (id, count, totalItems) => {},
-            // return false to prevent an item decrement
-            beforeDecrement: (id, itemCount) => {},
-            // return false to prevent an item increment
-            beforeIncrement: (id, itemCount) => {}
-          },
-    );
-  
-/*    $('.my-datepicker').datepicker([options]);*/
-  
+import './iq-dropdown'
+
+$(document).ready(function() {
+  $('.room').iqDropdown({
+    selectionText: ['спальня', 'кровать', 'ванная комната'],
+    textPlural: ['спальни', 'кровати', 'ванные комнаты'],
   });
+  $('.visitors').iqDropdown({
+    selectionText: 'гость',
+    textPlural: "гостя",
+    onChange: function(id, count, totalItems) {
+      console.log(id, count, totalItems);
+    },
+    beforeDecrement: function(id, itemCount) {
+      if (id === "adult") {
+        return itemCount.adult > itemCount.infant;
+      }
+      return true;
+    },
+    beforeIncrement: function(id, itemCount) {
+      if (id === "infant") {
+        return itemCount.adult > itemCount.infant;
+      }
+      return true;
+    }
+  });
+});
 
 
 
