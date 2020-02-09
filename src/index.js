d2483ec3d1f503be0179f1560e38560c958b58d8
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2020-02-09 23:52:49 +0300

    Started reorganizing the file structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // JS
-import './js/'
+//import './js/'
 import 'popper.js'
 import 'webpack-jquery-ui'
 import 'webpack-jquery-ui/css'
@@ -7,9 +7,8 @@ import 'webpack-jquery-ui/css'
 // import 'air-datepicker'
 
 // SCSS
-import './assets/scss/main.scss'
+//import './assets/scss/main.scss'
+import './pages/main.scss'
 
 // CSS (example)
 // import './assets/css/main.css'
-
-
