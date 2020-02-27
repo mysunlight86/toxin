d9e5c47bed088f56f5ea1207e6f6986b26e299f8
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2020-02-27 23:36:54 +0300

    Stylized datepicker, added date range

--- src/modules/datepicker/datepicker.js
+++ src/modules/datepicker/datepicker.js
@@ -1,24 +1,56 @@
 $(function() {
     // Инициализация
-    $(".arrival").datepicker({});
-    $(".exit").datepicker({});
+    $(".arrival").datepicker({
+        navTitles: {
+            days: "MM yyyy"
+        }
+    });
+    $(".exit").datepicker({
+        navTitles: {
+            days: "MM yyyy"
+        }
+    });
     $(".temp").datepicker({
-        inline: true
+        navTitles: {
+            days: "MM yyyy"
+        }
+    });
+    $(".period").datepicker({
+        navTitles: {
+            days: "MM yyyy"
+        },
+        range: true,
+        multipleDatesSeparator: " - ",
+        dateFormat: "dd M"
+    });
+    $(".none").datepicker({
+        inline: true,
+        navTitles: {
+            days: "MM yyyy"
+        },
+ //       showButtonPanel: true,
+        range: true,
+        multipleDatesSeparator: " - ",
+        clearButton: true,
+        showButtonPanel: true,
+ //       addButton: true,
+//        todayButton: new Date()
     });
 });
-
-// $('#my-element').datepicker([options])
-
-// Доступ к экземпляру объекта
-// $(".arrival").data("datepicker");
 /*
-$(function() {
-    $("#date")
-        .datepicker({
-            nextText: "",
-            prevText: "",
-            changeMonth: true,
-            changeYear: true
-        })
-        .mask("99/99/9999");
-});*/
+// Доступ к экземпляру объекта
+var myDatepicker = $(".mydatepicker").data("datepicker");
+//myDatepicker.hide();
+(function($) {
+    $.fn.datepicker = function(options) {
+        this.each(function() {
+            const $this = $(this);
+            const $panelButtons = $this.find(".datepicker--buttons");
+            var defaults = {
+                addButton: false
+            };
+        });
+
+        return this;
+    };
+})(jQuery);*/
