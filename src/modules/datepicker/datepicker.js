d3b44335276e68b633af09c7793d2693fed2f228
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2020-02-28 15:14:50 +0300

    Added date range selection to two text fields

--- src/modules/datepicker/datepicker.js
+++ src/modules/datepicker/datepicker.js
@@ -4,6 +4,8 @@ $(function() {
         navTitles: {
             days: "MM yyyy"
         }
+        // range: true,
+        // multipleDatesSeparator: " - "
     });
     $(".exit").datepicker({
         navTitles: {
@@ -28,15 +30,38 @@ $(function() {
         navTitles: {
             days: "MM yyyy"
         },
- //       showButtonPanel: true,
         range: true,
         multipleDatesSeparator: " - ",
-        clearButton: true,
-        showButtonPanel: true,
- //       addButton: true,
-//        todayButton: new Date()
+        clearButton: true
+        //       showButtonPanel: true,
+        //       addButton: true,
+        //        todayButton: new Date()
     });
 });
+
+// $(".arrival").datepicker({
+//     onSelect: function(fd, d, picker) {
+//         $(".arrival").val(fd.split("-")[0]);
+//         $(".exit").val(fd.split("-")[1]);
+//     }
+// });
+
+var $start = $(".start"),
+    $end = $(".end");
+
+$start.datepicker({
+    onSelect: function(fd, date) {
+        $end.data("datepicker").update("minDate", date);
+
+        $end.focus();
+    }
+});
+$end.datepicker({
+    onSelect: function(fd, date) {
+        $start.data("datepicker").update("maxDate", date);
+    }
+});
+
 /*
 // Доступ к экземпляру объекта
 var myDatepicker = $(".mydatepicker").data("datepicker");
@@ -54,3 +79,21 @@ var myDatepicker = $(".mydatepicker").data("datepicker");
         return this;
     };
 })(jQuery);*/
+
+$(".mydatepicker").datepicker({
+    onShow: function(input) {
+        setTimeout(function() {
+            const $panelButtons = $(".datepicker--buttons");
+            const $btnApply = $(
+                `<span class="datepicker--button" data-action="hide">
+                    применить
+                </span>`
+            );
+            $btnApply.unbind("click").bind("click", function() {
+                $.datepicker.onHide(input);
+            });
+
+            $btnApply.appendTo($panelButtons);
+        }, 1);
+    }
+});
